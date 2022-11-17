304bb13dd8cad4690f8eb4c68c73d835a552d722
yps <1106728884@qq.com>
2022-11-17 14:55:52 +0800

    项目创建

---
+++ README.md
@@ -0,0 +1,15 @@
+# 可拖拽菜单（vue2版）
+[**vue3版本**](https://github.com/QuietHear/vue-drag-menu-plus '浏览') | ***vue2版本*** 
+
+
+## 安装
+	npm i vue-drag-menu
+
+## 使用
+	import vueDragMenu from 'vue-drag-menu';
+	
+	Vue.use(vueDragMenu);
+
+
+## 1. 参数
+* `showBtn`：是否显示点击按钮-->Boolean;非必传;默认*true*
---
+++ lib/vueDragMenu.vue
@@ -0,0 +1,34 @@
+/*
+ * @Author: aFei
+ * @Date: 2022-11-17 14:27:47
+*/
+/*
+ * @LastEditors: aFei
+ * @LastEditTime: 2022-11-17 14:53:55
+ */
+<template>
+  <div :class="['vue-drag-menu', cname]">1</div>
+</template>
+<script>
+export default {
+  name: "vueDragMenu",
+  props: {
+    cname: {
+      // 自定义class
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<!--基础样式-->
+<style>
+@import "style/vueDragMenu.css";
+</style>
